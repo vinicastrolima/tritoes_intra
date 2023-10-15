9e59db19b16d2217c112b2b80217714391e114df
Vinícius Castro <vinicastrolimavcl@gmail.com>
2023-10-15 08:45:24 -0300

    Feat: Migration for Training

---
+++ database/migrations/2023_10_15_114342_create_training_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('training', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('location');
+            $table->date('date');
+            $table->time('time');
+            $table->boolean('validated')->default(false);
+            $table->text('comments')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('training');
+    }
+};
